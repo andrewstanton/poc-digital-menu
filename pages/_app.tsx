d3c842785e6e767eb649158901840eb034c0648f
webpandadesigns <andrew@toddprod.com>
2021-04-13 15:16:21 -0400

    DevOps setup

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from "next/app";
+// import { ThemeProvider } from "styled-components";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      {/* <ThemeProvider theme={theme}> */}
+      <Component {...pageProps} />
+      {/* </ThemeProvider> */}
+    </>
+  );
+}
